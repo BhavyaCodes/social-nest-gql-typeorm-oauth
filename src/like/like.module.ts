7671fadd72d5528a53ed43f01dd937315c69cea7
Bhavya <xbhavyatomar@gmail.com>
2021-08-26 21:21:42 +0530

    upgrade version, everything works

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -4,9 +4,10 @@ import { LikeResolver } from './like.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Like } from './entities/like.entity';
 import { User } from 'src/user/user.entity';
+import { Post } from 'src/post/entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Like, User])],
+  imports: [TypeOrmModule.forFeature([Like, User, Post])],
   providers: [LikeResolver, LikeService],
 })
 export class LikeModule {}
