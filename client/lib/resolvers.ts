8e98075647aebfe7309f8dff0a1eb8ddc5368f53
Bhavya <xbhavyatomar@gmail.com>
2021-08-27 14:17:28 +0530

    update schema in client

--- client/lib/resolvers.ts
+++ client/lib/resolvers.ts
@@ -8,17 +8,17 @@ const userProfile = {
   status: 'cached',
 };
 
-const Query: Required<QueryResolvers<ResolverContext>> = {
-  viewer(_parent, _args, _context, _info) {
-    return userProfile;
-  },
-};
+// const Query: Required<QueryResolvers<ResolverContext>> = {
+//   viewer(_parent, _args, _context, _info) {
+//     return userProfile;
+//   },
+// };
 
-const Mutation: Required<MutationResolvers<ResolverContext>> = {
-  updateName(_parent, _args, _context, _info) {
-    userProfile.name = _args.name;
-    return userProfile;
-  },
-};
+// const Mutation: Required<MutationResolvers<ResolverContext>> = {
+//   updateName(_parent, _args, _context, _info) {
+//     userProfile.name = _args.name;
+//     return userProfile;
+//   },
+// };
 
-export default { Query, Mutation };
+// export default { Query, Mutation };
