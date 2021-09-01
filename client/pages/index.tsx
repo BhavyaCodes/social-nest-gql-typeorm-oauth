0a5e34b1f083716ea2c700a4fd7ad3e264a41e44
Bhavya Tomar <58269749+BhavyaCodes@users.noreply.github.com>
2021-09-01 15:33:00 +0530

    Display posts on frontend (#2)
    * remove warning
    
    * upgrade version, everything works
    
    * add basic navbar
    
    * update apollo client on _app.tsx
    
    * add query to get all posts
    
    * update types and view posts

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,27 @@
+import { useQuery } from '@apollo/client';
 import Header from '../components/Header';
+import Posts from '../components/Posts';
+import {
+  GetAllPostsDocument,
+  GetAllPostsQuery,
+  GetAllPostsQueryResult,
+} from '../lib/queries.graphql';
+import { useGetAllPostsQuery } from '../__generated__/lib/queries.graphql';
 
 function index() {
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  console.log(apiBaseUrl);
+  const { loading, error, data } = useGetAllPostsQuery();
+  // const { loading, error, data } =
+  //   useQuery<GetAllPostsQuery>(GetAllPostsDocument);
+  // console.log(loading);
+  // console.log(error);
+  console.log(data);
+  if (!data) {
+    return <p>Loadiasdng...</p>;
+  }
   return (
     <div>
       <Header />
+      <Posts posts={data.allPosts} />
       Hello Next
     </div>
   );
