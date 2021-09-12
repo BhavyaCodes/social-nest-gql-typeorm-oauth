0186e6ffe85e4a7b1e0c8b795ee1aa3f72f1bf96
Bhavya <xbhavyatomar@gmail.com>
2021-09-13 00:28:28 +0530

    deleting working from frontend

--- client/components/Posts/Post.tsx
+++ client/components/Posts/Post.tsx
@@ -2,6 +2,7 @@ import { getUser } from '../../context/user.context';
 import {
   useDeletePostMutation,
   useLikePostMutation,
+  useUnlikePostMutation,
 } from '../../__generated__/lib/mutations.graphql';
 import { GetAllPostsDocument } from '../../__generated__/lib/queries.graphql';
 import { GetAllPostsQuery } from '../../__generated__/lib/queries.graphql';
@@ -14,7 +15,7 @@ export default function PostComponent({
   const { user } = getUser();
   const [deletePostMutation] = useDeletePostMutation();
   const [likePostMutation] = useLikePostMutation();
-  // const [likePostMutation2] = useLikePost2
+  const [unLikePostMutation] = useUnlikePostMutation();
 
   const deletePost = () => {
     console.log(post.id);
@@ -38,6 +39,16 @@ export default function PostComponent({
       .catch((e) => console.log(e));
   };
 
+  const handleUnLike = () => {
+    unLikePostMutation({
+      variables: {
+        unLikePostPostId: post.id,
+      },
+    })
+      .then((data) => console.log(data))
+      .catch((e) => console.log(e));
+  };
+
   return (
     <div style={{ border: '2px solid black' }}>
       <p>{post.user.name}</p>
@@ -47,16 +58,11 @@ export default function PostComponent({
         <button onClick={deletePost}>Delete Post</button>
       )}
       <p>hasLiked: {JSON.stringify(post.hasLiked)}</p>
-      {post.hasLiked === false ? (
+      {post.hasLiked === false && (
         <button onClick={handleLike}>Like Post</button>
-      ) : (
-        <button
-          onClick={() => {
-            console.log('unlike');
-          }}
-        >
-          Unlike Post
-        </button>
+      )}
+      {post.hasLiked === true && (
+        <button onClick={handleUnLike}>Unlike Post</button>
       )}
     </div>
   );
