0c7ed456d53fb2db5ef9b8694b11615ab68d23b5
Bhavya <xbhavyatomar@gmail.com>
2021-09-14 18:48:18 +0530

    add optimistic response to create post

--- client/components/Posts/Post.tsx
+++ client/components/Posts/Post.tsx
@@ -25,6 +25,7 @@ export default function PostComponent({
       },
       optimisticResponse: {
         deletePost: {
+          __typename: 'DeletedItem',
           id: post.id,
         },
       },
