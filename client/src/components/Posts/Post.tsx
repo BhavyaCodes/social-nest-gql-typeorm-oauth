7dc18011cd737fca7343960e60d08fcef0db0a45
Bhavya <xbhavyatomar@gmail.com>
2021-09-20 01:29:33 +0530

    view who liked post on frontend

--- client/src/components/Posts/Post.tsx
+++ client/src/components/Posts/Post.tsx
@@ -22,6 +22,7 @@ import { Menu, MenuItem } from '@mui/material';
 import { useState } from 'react';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
+import ViewLikes from './ViewLikes';
 
 export default function PostComponent({
   post,
@@ -35,6 +36,7 @@ export default function PostComponent({
   const [deletePostMutation] = useDeletePostMutation();
   const [likePostMutation] = useLikePostMutation();
   const [unLikePostMutation] = useUnlikePostMutation();
+  const [viewLikes, setViewLikes] = useState<boolean>(false);
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -134,6 +136,9 @@ export default function PostComponent({
     }).catch((e) => console.log(e));
   };
 
+  // const displayUsersWhoLikeThisPost = () => <ViewLikes postId={post.id} />;
+  const displayUsersWhoLikeThisPost = () => setViewLikes(true);
+
   return (
     <Card sx={{ my: 2 }}>
       <CardHeader
@@ -213,9 +218,12 @@ export default function PostComponent({
         </IconButton>
       </CardActions>
       {getLikeCountText(post.likeCount) && (
-        <Typography sx={{ mx: 2, mb: 1 }}>
+        <Typography sx={{ mx: 2, mb: 1 }} onClick={displayUsersWhoLikeThisPost}>
           {getLikeCountText(post.likeCount)}
         </Typography>
+      )}
+      {viewLikes && (
+        <ViewLikes postId={post.id} onClose={() => setViewLikes(false)} />
       )}
 
       {/* <p>LikeCount: {post.likeCount}</p> */}
